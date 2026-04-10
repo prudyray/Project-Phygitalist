0c0d8526d644acfc4f1e01fec8b2b7af6346d33a
Arun Kumar <arunkakorp@gmail.com>
2026-04-10 15:14:09 +0530

    fixed indentation issues

--- shop/apps/wishlist/context_processors.py
+++ shop/apps/wishlist/context_processors.py
@@ -2,9 +2,9 @@ def wishlists(request):
     if not (hasattr(request, 'user') and request.user.is_authenticated):
           return {}
 
-      count = request.session.get('_wishlist_count')
-      if count is None:
-          count = request.user.wishlists.count()
-          request.session['_wishlist_count'] = count
+    count = request.session.get('_wishlist_count')
+    if count is None:
+      count = request.user.wishlists.count()
+      request.session['_wishlist_count'] = count
 
-      return {"wishlists_count": count}
+    return {"wishlists_count": count}
