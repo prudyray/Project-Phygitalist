38ef8ce3047b93dc4ff7b056ed6fca8c4e530221
Arun Kumar <arunkakorp@gmail.com>
2026-04-10 14:59:49 +0530

    Tweaks to wishlist count context_processors

--- shop/apps/wishlist/context_processors.py
+++ shop/apps/wishlist/context_processors.py
@@ -1,5 +1,10 @@
 def wishlists(request):
-    if hasattr(request, 'user') and request.user.is_authenticated:
-        return { "wishlists": request.user.wishlists.all() }
+    if not (hasattr(request, 'user') and request.user.is_authenticated):
+          return {}
 
-    return {}
+      count = request.session.get('_wishlist_count')
+      if count is None:
+          count = request.user.wishlists.count()
+          request.session['_wishlist_count'] = count
+
+      return {"wishlists_count": count}
